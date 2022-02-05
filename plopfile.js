99efce813351f8c7509521acbe59cf435c833847
JesBrian <jesbrian666@gmail.com>
2022-02-05 21:32:44 +0800

    feat: init project

---
+++ plopfile.js
@@ -0,0 +1,5 @@
+const initComponent = require('./script/InitComponent')
+
+module.exports = (plop) => {
+  plop.setGenerator('Init Component', initComponent)
+}
--- script/InitComponent.js
+++ script/InitComponent.js
@@ -1,23 +1,30 @@
 const SOURCE_FILE_PATH = 'packages'
 
 module.exports = {
-  description: '创建文件，注意必须符合项目约束规范', // 命令启动描述
+  description: '初始化创建组件模板',
   prompts: [
     {
       type: 'input', // 命令方式，input-输入，list-选择，confirm-是否
-      name: 'fileName', // 输入的文件名
-      message: '请输入文件名称' // 提示信息
+      name: 'fileName',
+      message: '请输入初始化组件名称'
     }
   ],
   actions: data => {
-    const {fileName} = data // 拿到终端输入的文件名
+    const {fileName} = data
     const actions = []
     if (fileName) {
-      actions.push({ // 添加新的模板文件 add
+      actions.push({
         type: 'add',
-        path: `${SOURCE_FILE_PATH}/{{ fileName }}/index.vue`, // 生成的文件存放路径
-        templateFile: 'config/templates/index-vue.hbs', // 模板文件路径
-        data: { // 传递的数据给模板文件
+        path: `${SOURCE_FILE_PATH}/{{ fileName }}/index.ts`,
+        templateFile: 'templates/component/index-ts.hbs',
+        data: {
+          name: fileName,
+        }
+      }, {
+        type: 'add',
+        path: `${SOURCE_FILE_PATH}/{{ fileName }}/src/index.vue`,
+        templateFile: 'templates/component/index-vue.hbs',
+        data: {
           name: fileName,
         }
       })
